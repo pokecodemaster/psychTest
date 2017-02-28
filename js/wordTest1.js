9034fa2f3835349d91596e8749df8a98ad7520ba
cjrodriguez1 <0a:1f:a3:52:14:85:80:ed:ad:d6:9d:60:99:d5:46:da>
2017-02-27 20:59:26 -0500

    Added wordTest

---
+++ js/wordTest1.js
@@ -0,0 +1,86 @@
+var btnRed = document.getElementById("btn1");
+var btnBlue = document.getElementById("btn2");
+var btnOrange = document.getElementById("btn3");
+var btnGreen = document.getElementById("btn4");
+var Name = document.getElementById("fullName");
+
+
+var words = ["+-+-+-+-", "+-+-+-+-", "+-+-+-+-", "+-+-+-+-", "+-+-+-+-", "+-+-+-+-", "+-+-+-+-", "+-+-+-+-", "+-+-+-+-", "+-+-+-+-"];
+
+var colors = ["red","blue","green","orange"];
+
+
+var questions = {
+ 
+value: [
+    "Select Red",
+    "Select Green",
+    "Select Blue",
+    "Select Yellow",
+    "Select Red",
+    "Select Blue",
+    "Select Yellow"
+    ],
+answers: [
+                "red",
+    "red",
+    "blue",
+    "blue",
+    "orange",
+    "orange",
+    "green"
+    ],
+   
+}
+ 
+var index = 0;
+
+
+function startTest() {
+    displayWord();
+}
+
+
+function displayWord() {
+    var randColor = colors[Math.floor(Math.random() * colors.length)];
+    var randWord = words[Math.floor(Math.random() * words.length)];
+    document.getElementById("demo").style.color = randColor;
+    document.getElementById("demo").innerHTML = randWord;
+}
+ 
+ 
+
+function writeRed() {
+    var fireBaseRef = firebase.database().ref();
+
+    fireBaseRef.child("Person2").child("Practice Test").set("");
+    fireBaseRef.child("Person2").child("Practice Test").child("Word: coger").set("Displayed:RED, Selected:RED, Status:Correct, Time: 0:02");
+    fireBaseRef.child("Person2").child("Practice Test").child("Word: pendejo").set("Displayed:BLUE, Selected:RED, Status:Incorrect, Time: 0:03");
+    fireBaseRef.child("Person2").child("Practice Test").child("Word: mierda").set("Displayed:ORANGE, Selected:ORANGE, Status:Correct, Time: 0:01");
+    fireBaseRef.child("Person2").child("Practice Test").child("Word: mear").set("Displayed:GREEN, Selected:GREEN, Status:Incorrect, Time: 0:04");
+}
+ 
+ 
+function displayNextQuestion(value) {
+   
+}
+ 
+function logAnswer(value){
+                Participant.times.push(value);
+}
+ 
+function showResults() {
+                var mname = document.getElementById("fullName").value;
+                var allResults = "Results for " + Name.value + "<br>";
+    for(var i=0; i<Participant.times.length; i++)
+    {
+                allResults += questions.value[i] + ": " + Participant.times[i] + "<br>";
+    }
+    document.getElementById("results").innerHTML = allResults;
+    //window.location.href = "languageQuestionaire.html";
+}
+
+ 
+ 
+ 
+ 
