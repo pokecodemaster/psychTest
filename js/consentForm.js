3b1e11b42b8600983a62c77b2301d9162507e06b
Carlos Rodriguez <Carlos Rodriguez>
2017-03-23 01:33:34 +0100

    Writing to the db, created a key (sort of)

--- js/consentForm.js
+++ js/consentForm.js
@@ -1,1 +1,19 @@
-iiiiii
+var fireBaseRef = firebase.database().ref();
+
+var d = new Date();
+var KEY = d.getTime();
+
+
+
+
+function submitForm() {
+
+    var Name = document.getElementById("name").value;
+    var Age = document.getElementById("age").value;
+    fireBaseRef.child("<Name to UID> Map").child(Name).set(KEY);
+
+    window.location.href = '../html/participantDemographics.html';
+}
+
+
+
--- js/participantDemographics.js
+++ js/participantDemographics.js
@@ -1,1 +1,18 @@
-iiiiii
+
+function submitParticipantDemographics() {
+
+    
+    var Age = document.getElementById("age").value;
+    var Gender = document.getElementById("gender").value;
+    var CountryOfOrigin = document.getElementById("country").value;
+    var CountryOfFather = document.getElementById("countryFather").value;
+    var CountryOfMother = document.getElementById("countryMother").value;
+    var CountryReside = document.getElementById("currentCityState").value;
+
+    fireBaseRef.child(KEY).child("Participant Demographics").child("1 Age").set(Age);
+    fireBaseRef.child(KEY).child("Participant Demographics").child("2 Gender").set(Gender);
+    fireBaseRef.child(KEY).child("Participant Demographics").child("3 Country of Origin").set(CountryOfOrigin);
+    fireBaseRef.child(KEY).child("Participant Demographics").child("4 Country of Origin (Father)").set(CountryOfFather);
+    fireBaseRef.child(KEY).child("Participant Demographics").child("5 Country of Origin (Mother)").set(CountryOfMother);
+    fireBaseRef.child(KEY).child("Participant Demographics").child("6 Current City & State").set(CountryReside);
+}
