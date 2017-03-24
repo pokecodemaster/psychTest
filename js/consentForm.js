3f000ff47da8f802014a09726594d7c04ffbc078
Carlos Rodriguez <Carlos Rodriguez>
2017-03-24 02:33:21 +0100

    Added Auth, still working on the sign in

--- js/consentForm.js
+++ js/consentForm.js
@@ -3,16 +3,32 @@ var fireBaseRef = firebase.database().ref();
 var d = new Date();
 var KEY = d.getTime();
 
-
+localStorage.setItem("storageName", KEY);
 
 
 function submitForm() {
 
+    var email = "myemaizl@email.com";
+    var password = "mypassword";
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+        
+        console.log(error.code);
+        console.log(error.message);
+    });
+    console.log('winners and gonads');
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function () {
+        
+        console.log(error.code);
+        console.log(error.message);
+        alert('winners and gonads');
+    });
+    console.log('winners and gonadzz');
     var Name = document.getElementById("name").value;
     var Age = document.getElementById("age").value;
     fireBaseRef.child("<Name to UID> Map").child(Name).set(KEY);
 
-    window.location.href = '../html/participantDemographics.html';
+    window.location.href = 'participantDemographics.html';
 }
 
 
--- js/participantDemographics.js
+++ js/participantDemographics.js
@@ -1,6 +1,8 @@
 
 function submitParticipantDemographics() {
-
+    
+    
+    var uKey = KEY;
     
     var Age = document.getElementById("age").value;
     var Gender = document.getElementById("gender").value;
@@ -9,10 +11,12 @@ function submitParticipantDemographics() {
     var CountryOfMother = document.getElementById("countryMother").value;
     var CountryReside = document.getElementById("currentCityState").value;
 
-    fireBaseRef.child(KEY).child("Participant Demographics").child("1 Age").set(Age);
-    fireBaseRef.child(KEY).child("Participant Demographics").child("2 Gender").set(Gender);
-    fireBaseRef.child(KEY).child("Participant Demographics").child("3 Country of Origin").set(CountryOfOrigin);
-    fireBaseRef.child(KEY).child("Participant Demographics").child("4 Country of Origin (Father)").set(CountryOfFather);
-    fireBaseRef.child(KEY).child("Participant Demographics").child("5 Country of Origin (Mother)").set(CountryOfMother);
-    fireBaseRef.child(KEY).child("Participant Demographics").child("6 Current City & State").set(CountryReside);
+    fireBaseRef.child(uKey).child("Participant Demographics").child("1 Age").set(Age);
+    fireBaseRef.child(uKey).child("Participant Demographics").child("2 Gender").set(Gender);
+    fireBaseRef.child(uKey).child("Participant Demographics").child("3 Country of Origin").set(CountryOfOrigin);
+    fireBaseRef.child(uKey).child("Participant Demographics").child("4 Country of Origin (Father)").set(CountryOfFather);
+    fireBaseRef.child(uKey).child("Participant Demographics").child("5 Country of Origin (Mother)").set(CountryOfMother);
+    fireBaseRef.child(uKey).child("Participant Demographics").child("6 Current City & State").set(CountryReside);
+
+    window.location.href = 'continuePage.html';
 }
